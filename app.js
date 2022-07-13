065220409f593baa28338b46c674502269a9f054
Rafael Langoni <langoninaves@gmail.com>
2022-07-12 21:41:26 -0300

    projeto finalizado; implementacao de logica para cores

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const botao = document.querySelector("[data-tipo=botao]");
+let corDeFundo = document.querySelector(".principal__main");
+let paragrafo = document.querySelector(".principal__cor");
 const hexaDecimal = [0, 1, 2, 3, 4, 5, 6, "A", "B", "C", "D", "E", "F"];
+
+
+/**
+ * 
+ * @returns Gera número aleatório para ser utilizado no índice do for, 
+ * limitado a quantidade de itens doa array de cores
+ */
+function numeroAleatorio(){
+    return Math.floor(Math.random() * hexaDecimal.length);
+}
+
+/**
+ * Cor hexadecimal sempre tem limite máximo de 6 posições, mais # no começo
+ * A variável corHexa acumula o valor recebido da posição aleatória do array,
+ * gerando cores aleatórias a cada loop
+ */
+function criaCor(){
+    let corHexa = "#";
+    for (let index = 0; index < 6; index++) {
+        corHexa += hexaDecimal[numeroAleatorio()]
+    }
+    corDeFundo.style.backgroundColor = corHexa;
+    paragrafo.textContent = corHexa;
+}
 
 botao.addEventListener("click", criaCor);
 
-function criaCor(){
-    for (let index = 0; index < hexaDecimal.length; index++) {
-        let element = hexaDecimal[index];
-        console.log(element)
-    }
-}
+
+
 
